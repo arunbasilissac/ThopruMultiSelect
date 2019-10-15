26a252e43aeb9bfb34d6f83ed162bf31960254e9
arun.basil <arun.basil@innovaturelabs.com>
2019-10-15 12:27:07 +0530

    Initial Setup

---
+++ ThopruMultiSelect/ThopruMultiSelect.h
@@ -0,0 +1,19 @@
+//
+//  ThopruMultiSelect.h
+//  ThopruMultiSelect
+//
+//  Created by Arun Basil Issac on 15/10/19.
+//  Copyright © 2019 pinmicro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThopruMultiSelect.
+FOUNDATION_EXPORT double ThopruMultiSelectVersionNumber;
+
+//! Project version string for ThopruMultiSelect.
+FOUNDATION_EXPORT const unsigned char ThopruMultiSelectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThopruMultiSelect/PublicHeader.h>
+
+
---
+++ ThopruMultiSelectExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThopruMultiSelectExample
+//
+//  Created by Arun Basil Issac on 15/10/19.
+//  Copyright © 2019 pinmicro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
